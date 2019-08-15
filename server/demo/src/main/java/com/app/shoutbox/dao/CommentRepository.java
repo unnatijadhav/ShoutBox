14933c09e55127c69bc59b3c0f47db68b9df2e9a
unnatijadhav <53641854+unnatijadhav@users.noreply.github.com>
2019-08-15 14:18:27 +0530

    Update CommentRepository.java

--- server/demo/src/main/java/com/app/shoutbox/dao/CommentRepository.java
+++ server/demo/src/main/java/com/app/shoutbox/dao/CommentRepository.java
@@ -10,15 +10,7 @@ import com.app.shoutbox.model.Shouts;
 
 public interface CommentRepository extends JpaRepository<Comments, Integer> {
 
-	/*
-	 * @Query("select c from Comments c where c.shout like ?1") public
-	 * List<Comments> findCommentsByShout(Shouts currentShoutId);
-	 */
-	/*
-	 * @Query("select c from Comments c where c.shout =: currentShout and c.user in ("
-	 * + "select u from User u where u.userActive = 1)") public List<Comments>
-	 * getCommentsByShoutAndIsUserActive(Shouts currentShout);
-	 */
+
 	@Query("select c from Comments c where c.shout like ?1 and c.user in ("
 			+ "select u from User u where u.userActive = 1)")
 	public List<Comments> getCommentsByShoutAndIsUserActive(Shouts currentShout);
