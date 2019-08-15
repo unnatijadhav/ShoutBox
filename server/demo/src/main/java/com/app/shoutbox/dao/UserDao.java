3eea29e923936e3824e610d370124625c41a7832
unnatijadhav <53641854+unnatijadhav@users.noreply.github.com>
2019-08-15 14:26:57 +0530

    Update UserDao.java

--- server/demo/src/main/java/com/app/shoutbox/dao/UserDao.java
+++ server/demo/src/main/java/com/app/shoutbox/dao/UserDao.java
@@ -7,19 +7,10 @@ import com.app.shoutbox.model.User;
 
 public interface UserDao {
 
-	/*
-	 * AUTHOR: Prashant Sharma
-	*/
 	public User registerUser(User u);
 	public Integer verifyMail(String mail);
 	public List<Shouts> getCurrentUserShouts(User user);
 	public User getUserById(int id);
-	
-
-	/*
-	 * AUTHOR: Unnati Jadhav
-	*/
-	public User getCurrentUserById(Integer currentUserId); 
-	
+	public User getCurrentUserById(Integer currentUserId); 	
 	public User loginUser(User u);
 }
